d2901c3ab671a5b7a2afc3851f404a2a1f6d4ac4
akshatbjain <akshatbjain93@gmail.com>
2016-11-25 03:23:45 -0500

    Camshift first attempt

---
+++ cam_shift_akshat.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import operator
+
+cam = cv2.VideoCapture(0)
+refPt = []
+Mode = 0
+ranges = [[0, 180], [0, 255], [0, 255]]
+thresh = [0, 0, 0]
+
+def get_roi(event, x, y, flags, param):
+    global refPt
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        refPt.append((x, y))
+
+        cv2.rectangle(img, refPt[0], refPt[1], (0, 255, 0), 2)
+        cv2.imshow('Camshift', img)
+
+cv2.namedWindow('Camshift')
+cv2.setMouseCallback('Camshift', get_roi)
+
+while(1):
+    ret, img = cam.read()
+    duplicate = img.copy()
+    hsv_img = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV)
+
+    if Mode == 0:
+        cv2.imshow('Camshift', img)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('p'):
+            cv2.waitKey(0)
+
+        elif key == ord('d'):
+            break
+
+        elif key == ord('r'):
+            img = duplicate.copy()
+
+
+    if ((len(refPt) == 2) and (Mode == 0)):
+        Mode = 1
+        roi = duplicate[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        cv2.imshow('ROI', roi)
+
+        hsv = cv2.cvtColor(roi.copy(), cv2.COLOR_BGR2HSV)
+        #mask = np.zeros((hsv.shape[0], hsv.shape[1]))
+        #mask = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
+        #hist = cv2.calcHist([hsv], [0], None , [180], [0, 180])
+        color = ['b', 'g', 'r']
+
+        for i in range(3):
+            histr = cv2.calcHist([hsv],[i],None,[ranges[i][1]],ranges[i])
+            print histr.shape
+            #cv2.normalize(histr, histr, ranges[i][0], ranges[i][1], cv2.NORM_MINMAX);
+
+            #min_index, min_value = min(enumerate(values), key=operator.itemgetter(1))
+            thresh[i], max_value = max(enumerate(histr), key=operator.itemgetter(1))
+
+            #thresh[i] = histr.index(max(histr))
+            print thresh[i]
+
+            #plt.plot(histr,color = color[i])
+            #plt.xlim([0,256])
+        #plt.show()
+    if Mode == 1:
+        #Creating arrays to store the min and max HSV ranges
+        lower = np.array([thresh[0]-20, thresh[1]-20, thresh[2]-20])
+        upper = np.array([thresh[0]+20, thresh[1]+20, thresh[2]+20])
+
+        #Create binary mask of in range object
+        mask = cv2.inRange(hsv_img, lower, upper)
+
+        #Bitwise AND operation on orginal image using mask
+        #obj = cv2.bitwise_and(img, img, mask=mask)
+
+        cv2.imshow('Mask', mask)
+
+        #cv2.imshow('Object', obj)
+        k = cv2.waitKey(1) & 0xFF
+        if(k==27):
+            break
